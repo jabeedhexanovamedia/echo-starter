962a91093fecef1b10c74aee0fdd8bec6cff9913
Syed Jabeed <jabeedsyedinfo@gmail.com>
2026-01-05 16:53:37 +0530

    feat: CORS and Rate limiter

--- internal/server/server.go
+++ internal/server/server.go
@@ -7,14 +7,31 @@ import (
 	"github.com/jabeedhexanovamedia/echo-starter/internal/handler"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/time/rate"
 )
 
 func New(cfg *config.Config) *echo.Echo {
 	e := echo.New()
 
 	// Global middleware
-	e.Use(middleware.Logger())
+	e.Use(middleware.RequestLogger())
 	e.Use(middleware.Recover())
+
+	// CORS
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	}))
+
+	// Rate Limiting
+	ratePerSecond := float64(cfg.RateLimiter.Requests) / cfg.RateLimiter.Duration.Seconds()
+	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStoreWithConfig(
+		middleware.RateLimiterMemoryStoreConfig{
+			Rate:      rate.Limit(ratePerSecond),
+			Burst:     cfg.RateLimiter.Requests,
+			ExpiresIn: cfg.RateLimiter.Duration,
+		},
+	)))
 
 	// Register routes
 	e.GET("/", handler.Health)
